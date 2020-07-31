a9b80ddab98230b2c73bd4014c91967a3e206db7
Mike Brasher <mikbras@microsoft.com>
2020-07-31 17:51:48 +0000

    empty enclave

---
+++ tests/empty/enc.c
@@ -0,0 +1,18 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#include <openenclave/enclave.h>
+#include "run_t.h"
+
+int run_ecall(void)
+{
+    return 0;
+}
+
+OE_SET_ENCLAVE_SGX(
+    1,    /* ProductID */
+    1,    /* SecurityVersion */
+    true, /* Debug */
+    16*4096, /* NumHeapPages */
+    4096, /* NumStackPages */
+    2);   /* NumTCS */
