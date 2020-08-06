6e1f830714c398cc84e77a3ce68ccef65c086b5d
Mike Brasher <mikbras@microsoft.com>
2020-08-06 15:32:13 +0000

    add libos/syscallext.h

---
+++ include/libos/syscallext.h
@@ -0,0 +1,16 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#ifndef _LIBOS_SYSCALLEXT_H
+#define _LIBOS_SYSCALLEXT_H
+
+#include <libos/types.h>
+
+/* libos-specific syscalls */
+
+#define SYS_libos_trace 1001
+#define SYS_libos_trace_ptr 1002
+#define SYS_libos_dump_stack 1003
+#define SYS_libos_dump_ehdr 1004
+
+#endif /* _LIBOS_SYSCALLEXT_H */
