1e7cc62c10ddcace7ab72267a99e14b794105937
Mike Brasher <mikbras@microsoft.com>
2020-08-11 00:09:17 +0000

    add getcwd() example

--- samples/getcwd/main.c
+++ samples/getcwd/main.c
@@ -9,14 +9,7 @@ int main(int argc, const char* argv[])
     char buf[PATH_MAX];
 
     getcwd(buf, sizeof(buf));
-
     printf("cwd={%s}\n", buf);
-
-    void* p = malloc(16*1024);
-
-    memset(p, 0xdd, 16*1024);
-
-    free(p);
 
     return 0;
 }
