bd91458069308076e3e084f66bc154ab9e25f7f9
Mike Brasher <mikbras@microsoft.com>
2020-08-22 00:08:29 +0000

    update dlopen sample

--- samples/dlopen/appdir/foo.c
+++ samples/dlopen/appdir/foo.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+
+void call_bar(void)
+{
+    void* handle;
+    typedef void (*func_t)();
+    func_t func;
+    const char path[] = "/libbar.so";
+
+    if (!(handle = dlopen(path, RTLD_NOW)))
+    {
+        fprintf(stderr, "libfoo: dlopen() failed\n");
+        exit(1);
+    }
+
+    if (!(func = dlsym(handle, "bar")))
+    {
+        fprintf(stderr, "libfoo: dlsym() failed\n");
+        exit(1);
+    }
+
+    (*func)();
+
+    dlclose(handle);
+}
 
 void foo()
 {
     printf("foo()\n");
+    call_bar();
 }
