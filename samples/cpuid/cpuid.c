97a3d34bbc2b471170ec5af69b6166fd7bc318b2
Mike Brasher <mikbras@microsoft.com>
2020-08-25 16:08:15 +0000

    added cpuid sample

---
+++ samples/cpuid/cpuid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <cpuid.h>
+
+int main(int argc, const char* argv[])
+{
+    const uint32_t leaf = 0xC0000000;
+    const uint32_t subleaf = 0;
+    uint32_t rax = 0;
+    uint32_t rbx = 0;
+    uint32_t rcx = 0;
+    uint32_t rdx = 0;
+
+    __cpuid_count(leaf, subleaf, rax, rbx, rcx, rdx);
+
+    printf("rax=%x rbx=%x rcx=%x rdx=%x\n", rax, rbx, rcx, rdx);
+
+    return 0;
+}
