e24637d49349b98070c627d18dce842b0466586f
Mike Brasher <mikbras@microsoft.com>
2020-08-27 17:06:15 +0000

    fix build break

--- host/malloc.c
+++ host/malloc.c
@@ -8,17 +8,32 @@ void libos_free(void* ptr)
     return free(ptr);
 }
 
-void* libos_calloc(size_t nmemb, size_t size)
+void* __libos_calloc(
+    size_t nmemb,
+    size_t size,
+    const char* file,
+    size_t line,
+    const char* func)
 {
     return calloc(nmemb, size);
 }
 
-void* libos_realloc(void* ptr, size_t size)
+void* __libos_realloc(
+    void* ptr,
+    size_t size,
+    const char* file,
+    size_t line,
+    const char* func)
 {
     return realloc(ptr, size);
 }
 
-void* libos_memalign(size_t alignment, size_t size)
+void* __libos_memalign(
+    size_t alignment,
+    size_t size,
+    const char* file,
+    size_t line,
+    const char* func)
 {
     return memalign(alignment, size);
 }
