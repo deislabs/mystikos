f6276ffd3b78337b3dd5513848b2eab09491460d
Mike Brasher <mikbras@microsoft.com>
2020-09-11 18:53:00 +0000

    cleanup <libos/types.h>

--- include/libos/types.h
+++ include/libos/types.h
@@ -4,35 +4,10 @@
 #include <limits.h>
 #include <stddef.h>
 #include <stdint.h>
-
-#ifndef NULL
-#define NULL ((void*)0)
-#endif
+#include <stdbool.h>
+#include <sys/types.h>
 
 #define LIBOS_PAGE_SIZE 4096
-
-typedef long ssize_t;
-typedef long off_t;
-typedef unsigned long size_t;
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef short int16_t;
-typedef unsigned short uint16_t;
-typedef int int32_t;
-typedef unsigned int uint32_t;
-typedef long int64_t;
-typedef unsigned long uint64_t;
-typedef unsigned long uintptr_t;
-typedef long ptrdiff_t;
-typedef long intptr_t;
-typedef long time_t;
-typedef long suseconds_t;
-
-#ifndef __cplusplus
-#define true 1
-#define false 0
-#define bool _Bool
-#endif
 
 typedef struct _libos_path
 {
