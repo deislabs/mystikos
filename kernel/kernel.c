791f7e9bfc105a1f50c59a61e690119bb9453943
Mike Brasher <mikbras@microsoft.com>
2020-09-18 19:58:25 +0000

    forgot these two files

---
+++ kernel/kernel.c
@@ -0,0 +1,3 @@
+#include <libos/kernel.h>
+
+libos_kernel_args_t __libos_kernel_args;
---
+++ kernel/memconfig.c
@@ -0,0 +1,27 @@
+#include <libos/tcall.h>
+#include <libos/defs.h>
+#include <libos/strings.h>
+#include <libos/kernel.h>
+
+static void _dump_target_stat(void)
+{
+    libos_target_stat_t config;
+    const libos_kernel_args_t* args = &__libos_kernel_args;
+
+    if (!args)
+        libos_panic("bad argument");
+
+    if (libos_tcall_target_stat(&config) != 0)
+        libos_panic("libos_tcall_target_stat() failed");
+
+    libos_printf("=== libos memory configuration:\n");
+    libos_printf("kernel_mem_size=%zu\n", config.heap_size);
+    libos_printf("user_mem_size=%zu\n", args->mman_size);
+    libos_printf("rootfs_size=%zu\n", args->rootfs_size);
+    libos_printf("crt_size=%zu\n", args->crt_size);
+}
+
+LIBOS_WEAK_ALIAS(_dump_target_stat, libos_dump_target_stat);
+
+/* shorten name to make it convenient to call from debugger */
+LIBOS_WEAK_ALIAS(_dump_target_stat, memconf);
