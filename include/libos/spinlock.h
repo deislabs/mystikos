82c98f00d250257197098bb11645456645be049a
Mike Brasher <mikbras@microsoft.com>
2020-09-18 07:07:48 +0000

    switch to builtin spinlocks

--- include/libos/spinlock.h
+++ include/libos/spinlock.h
@@ -1,12 +1,50 @@
 #ifndef _LIBOS_SPINLOCK_H
 #define _LIBOS_SPINLOCK_H
 
+#include <errno.h>
 #include <libos/defs.h>
 #include <libos/types.h>
 
 #define LIBOS_SPINLOCK_INITIALIZER 0
 
-typedef volatile uint32_t libos_spinlock_t;
+#define LIBOS_SPINLOCK_BUILTINS
+
+/*
+**==============================================================================
+**
+** LIBOS_SPINLOCK_BUILTINS: based on gcc builtin atomics
+**
+**==============================================================================
+*/
+
+#ifdef LIBOS_SPINLOCK_BUILTINS
+
+typedef volatile int libos_spinlock_t;
+
+LIBOS_INLINE void libos_spin_lock(libos_spinlock_t* s)
+{
+    while (*(volatile int*)s || __sync_val_compare_and_swap(s, 0, 1))
+        __asm__ __volatile__("pause" : : : "memory");
+}
+
+LIBOS_INLINE void libos_spin_unlock(libos_spinlock_t* s)
+{
+    __sync_lock_test_and_set(s, 0);
+}
+
+#endif /* LIBOS_SPINLOCK_BUILTINS */
+
+/*
+**==============================================================================
+**
+** LIBOS_SPINLOCK_ASSEMBLY: based on inline assembly
+**
+**==============================================================================
+*/
+
+#ifdef LIBOS_SPINLOCK_ASSEMBLY
+
+typedef volatile int libos_spinlock_t;
 
 LIBOS_INLINE void libos_spin_lock(libos_spinlock_t* spinlock)
 {
@@ -41,5 +79,7 @@ LIBOS_INLINE void libos_spin_unlock(libos_spinlock_t* spinlock)
                  : "r"(LIBOS_SPINLOCK_INITIALIZER), "m"(*spinlock) /* %1 */
                  : "memory");
 }
+
+#endif /* LIBOS_SPINLOCK_ASSEMBLY */
 
 #endif /* _LIBOS_SPINLOCK_H */
