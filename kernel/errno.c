8ae37604637047837768e23bd248a37ed1e0b224
Mike Brasher <mikbras@microsoft.com>
2020-09-25 22:17:01 +0000

    add missing file

---
+++ kernel/errno.c
@@ -0,0 +1,14 @@
+#include <errno.h>
+
+#include <libos/thread.h>
+#include <libos/tcall.h>
+
+int* __errno_location(void)
+{
+    int* ptr = NULL;
+
+    libos_assume(libos_tcall_get_errno_location(&ptr) == 0);
+    libos_assume(ptr != 0);
+
+    return ptr;
+}
