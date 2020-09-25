9d610e124d82b79aa71e6744e181920ac7275edd
Mike Brasher <mikbras@microsoft.com>
2020-09-25 22:18:40 +0000

    add once.h

---
+++ kernel/once.c
@@ -0,0 +1,38 @@
+#include <libos/once.h>
+
+int libos_once(libos_once_t* once, void (*func)(void))
+{
+    const uint64_t busy = 1;
+    const uint64_t done = 2;
+
+    if (!once || !func)
+        return -EINVAL;
+
+    libos_once_t status = __atomic_load_n(once, __ATOMIC_ACQUIRE);
+
+    if (status != done)
+    {
+        libos_once_t expected = 0;
+
+        bool ret = __atomic_compare_exchange_n(
+            once,
+            &expected,
+            busy,
+            false,
+            __ATOMIC_RELEASE,
+            __ATOMIC_ACQUIRE);
+
+        if (ret)
+        {
+            func();
+            __atomic_store_n(once, done, __ATOMIC_RELEASE);
+        }
+        else
+        {
+            while (__atomic_load_n(once, __ATOMIC_ACQUIRE) != done)
+                asm volatile("pause" ::: "memory");
+        }
+    }
+
+    return 0;
+}
