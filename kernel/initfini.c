1d610210092f4a159be53d4888e9c82f7e348d72
Mike Brasher <mikbras@microsoft.com>
2020-09-27 16:04:57 +0000

    remove unused liboskenel.a

--- kernel/initfini.c
+++ kernel/initfini.c
@@ -1,9 +1,26 @@
 #include <libos/initfini.h>
+#include <libos/strings.h>
 
-__attribute__((weak)) void libos_call_init_functions(void)
+void libos_call_init_functions(void)
 {
+    void (**fn)(void);
+    extern void (*__init_array_start)(void);
+    extern void (*__init_array_end)(void);
+
+    for (fn = &__init_array_start; fn < &__init_array_end; fn++)
+    {
+        (*fn)();
+    }
 }
 
-__attribute__((weak)) void libos_call_fini_functions(void)
+void libos_call_fini_functions(void)
 {
+    void (**fn)(void);
+    extern void (*__fini_array_start)(void);
+    extern void (*__fini_array_end)(void);
+
+    for (fn = &__fini_array_end - 1; fn >= &__fini_array_start; fn--)
+    {
+        (*fn)();
+    }
 }
