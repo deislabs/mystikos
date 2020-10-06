ba60dff186185e5698895428506e493a632d8822
Mike Brasher <mikbras@microsoft.com>
2020-10-06 17:12:29 -0500

    verify that strings are zero terminated (#91)
    Signed-off-by: Mike Brasher <mikbras@microsoft.com>

--- tests/args/args.c
+++ tests/args/args.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 void dump(const char* args[], size_t size)
 {
@@ -16,7 +17,7 @@ void dump(const char* args[], size_t size)
     printf("\n");
 }
 
-void test(void)
+void test_pack_unpack(void)
 {
     libos_args_t args;
 
@@ -99,9 +100,124 @@ void test(void)
     free(packed_data);
 }
 
+void test_missing_null_terminator(void)
+{
+    libos_args_t args;
+
+    assert(libos_args_init(&args) == 0);
+    assert(args.size == 0);
+    assert(args.data[0] == NULL);
+
+    assert(libos_args_append(&args, NULL, 0) == 0);
+
+    assert(libos_args_append1(&args, "arg0") == 0);
+    assert(args.size == 1);
+
+    assert(strcmp(args.data[0], "arg0") == 0);
+    assert(args.data[1] == NULL);
+
+    assert(libos_args_append1(&args, "arg1") == 0);
+    assert(args.size == 2);
+
+    assert(strcmp(args.data[1], "arg1") == 0);
+    assert(args.data[2] == NULL);
+
+    void* packed_data;
+    size_t packed_size;
+    assert(libos_args_pack(&args, &packed_data, &packed_size) == 0);
+    assert(packed_data != NULL);
+    assert(packed_size != 0);
+
+    /* replace the null terminator of "arg1" with "x" */
+    {
+        bool found = false;
+        uint8_t* start = packed_data;
+        uint8_t* end = (uint8_t*)packed_data + packed_size;
+
+        for (uint8_t* p = start; p != end; p++)
+        {
+            if (memcmp(p, "arg1", 5) == 0)
+            {
+                p[4] = 'x';
+                found = true;
+                break;
+            }
+        }
+
+        assert(found);
+    }
+
+    /* unpacking should detect the missing null byte and fail */
+    libos_args_t out;
+    assert(libos_args_unpack(&out, packed_data, packed_size) == -1);
+
+    free(args.data);
+    free(packed_data);
+}
+
+void test_bounds_violation(void)
+{
+    libos_args_t args;
+
+    assert(libos_args_init(&args) == 0);
+    assert(args.size == 0);
+    assert(args.data[0] == NULL);
+
+    assert(libos_args_append(&args, NULL, 0) == 0);
+
+    assert(libos_args_append1(&args, "arg0") == 0);
+    assert(args.size == 1);
+
+    assert(strcmp(args.data[0], "arg0") == 0);
+    assert(args.data[1] == NULL);
+
+    void* packed_data;
+    size_t packed_size;
+    assert(libos_args_pack(&args, &packed_data, &packed_size) == 0);
+    assert(packed_data != NULL);
+    assert(packed_size != 0);
+
+    /* enlarge the packed data by one byte */
+    assert((packed_data = realloc(packed_data, packed_size + 1)) != NULL);
+
+    /* replace "arg0" with a longer string that extends beyond the end of buf */
+    {
+        bool found = false;
+        uint8_t* start = packed_data;
+        uint8_t* end = (uint8_t*)packed_data + packed_size;
+
+        for (uint8_t* p = start; p != end; p++)
+        {
+            const size_t r = end - p;
+
+            if (memcmp(p, "arg0", 5) == 0)
+            {
+                memset(p, 'x', r + 1);
+                p[r] = '\0';
+
+                /* change the size to be one larger than the remaining bytes */
+                *((size_t*)p - 1) = r + 1;
+                found = true;
+                break;
+            }
+        }
+
+        assert(found);
+    }
+
+    /* unpacking should detect the missing null byte and fail */
+    libos_args_t out;
+    assert(libos_args_unpack(&out, packed_data, packed_size) == -1);
+
+    free(args.data);
+    free(packed_data);
+}
+
 int main(int argc, const char* argv[])
 {
-    test();
+    test_pack_unpack();
+    test_missing_null_terminator();
+    test_bounds_violation();
 
     printf("=== passed test (%s)\n", argv[0]);
 
