c08bbe867ba0ab6f33fe0cba86c6ac894f6187d1
Vikas Tikoo <vitikoo@microsoft.com>
2021-03-11 20:22:13 +0000

    Cleanup /proc/[pid] on process thread exit
    Signed-off-by: Vikas Tikoo <vitikoo@microsoft.com>

--- include/myst/procfs.h
+++ include/myst/procfs.h
@@ -4,6 +4,8 @@
 #ifndef _MYST_PROCFS_H
 #define _MYST_PROCFS_H
 
+#include <fcntl.h>
+
 #include <myst/ramfs.h>
 
 int create_proc_root_entries();
@@ -19,5 +21,8 @@ int create_proc_root_entries();
 int procfs_setup();
 
 int procfs_teardown();
+
+/* Cleanup /proc/[pid] entries */
+int procfs_pid_cleanup(pid_t pid);
 
 #endif /* _MYST_PROCFS_H */
