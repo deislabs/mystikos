d27e1e5c5b005fa60fcb8fd0c229b59c7150d86d
Xuejun Yang <xuejya@microsoft.com>
2021-03-17 00:32:47 +0000

    Use gcompat for a more compatible layer with glibc
    Signed-off-by: Xuejun Yang <xuejya@microsoft.com>
    
    Use gcompat for a more compatible layer with glibc

--- crt/glibc/__vprintf_chk.c
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-#include <stdio.h>
-
-int __vprintf_chk(int flag, const char* format, va_list ap)
-{
-    return __vfprintf_chk(stdout, flag, format, ap);
-}
