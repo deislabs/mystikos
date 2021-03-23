af2026d633ba6289f8abf62fb127f3b4763f692e
Mike Brasher <mikbras@microsoft.com>
2021-03-23 20:13:41 +0000

    posix_spawn file actions test

---
+++ tests/spawnfa/child.c
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main(int argc, const char* argv[], const char* envp[])
+{
+    printf("abcdefghijklmnopqrstuvwxyz");
+    return 99;
+}
