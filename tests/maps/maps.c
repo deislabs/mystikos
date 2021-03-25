9af3fa1302a1f30525835d1307dbf01079197ed8
Mike Brasher <mikbras@microsoft.com>
2021-03-25 19:20:08 +0000

    regions: add maps test

---
+++ tests/maps/maps.c
@@ -0,0 +1,87 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <myst/maps.h>
+
+int main()
+{
+    const size_t PAGE_SIZE = 4096;
+    size_t length = 8 * PAGE_SIZE;
+    const int prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+    const int flags = MAP_ANONYMOUS | MAP_PRIVATE;
+    myst_maps_t* maps;
+    int r;
+
+    uint8_t* addr = mmap(NULL, length, prot, flags, -1, 0);
+    printf("addr=%p\n", addr);
+    mprotect(addr + 0 * PAGE_SIZE, PAGE_SIZE, PROT_NONE);
+    mprotect(addr + 1 * PAGE_SIZE, PAGE_SIZE, PROT_READ);
+    mprotect(addr + 2 * PAGE_SIZE, PAGE_SIZE, PROT_WRITE);
+    mprotect(addr + 3 * PAGE_SIZE, PAGE_SIZE, PROT_EXEC);
+    mprotect(addr + 4 * PAGE_SIZE, PAGE_SIZE, PROT_READ | PROT_WRITE);
+    mprotect(addr + 5 * PAGE_SIZE, PAGE_SIZE, PROT_READ | PROT_EXEC);
+    mprotect(addr + 6 * PAGE_SIZE, PAGE_SIZE, PROT_WRITE | PROT_EXEC);
+
+    if ((r = myst_maps_load(&maps)) != 0)
+    {
+        fprintf(stderr, "load_maps() failed: errno=%d\n", -r);
+        exit(1);
+    }
+
+    myst_maps_dump(maps);
+
+    for (size_t i = 0; i < (length / PAGE_SIZE); i++)
+    {
+        uint8_t* p = addr + (i * PAGE_SIZE);
+        struct myst_mstat ms;
+        myst_mstat(maps, p, &ms);
+        myst_mstat_dump(&ms);
+
+        assert(ms.flags == MAP_PRIVATE);
+
+        switch (i)
+        {
+            case 0:
+                assert(ms.prot == PROT_NONE);
+                break;
+            case 1:
+                assert(ms.prot == PROT_READ);
+                break;
+            case 2:
+                assert(ms.prot == PROT_WRITE);
+                break;
+            case 3:
+                assert(ms.prot == PROT_EXEC);
+                break;
+            case 4:
+                assert(ms.prot == (PROT_READ | PROT_WRITE));
+                break;
+            case 5:
+                assert(ms.prot == (PROT_READ | PROT_EXEC));
+                break;
+            case 6:
+                assert(ms.prot == (PROT_WRITE | PROT_EXEC));
+                break;
+            case 7:
+                assert(ms.prot == (PROT_READ | PROT_WRITE | PROT_EXEC));
+                break;
+        }
+    }
+
+    myst_maps_free(maps);
+    munmap(addr, length);
+
+    return 0;
+}
