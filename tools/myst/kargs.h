08124d88478678bdae1277fb39dcb96023815933
Mike Brasher <mikbras@microsoft.com>
2021-03-28 17:02:22 +0000

    add tools/myst/kargs.h

---
+++ tools/myst/kargs.h
@@ -0,0 +1,30 @@
+#ifndef _MYST_MYST_KARGS_H
+#define _MYST_MYST_KARGS_H
+
+#include <myst/kernel.h>
+
+int init_kernel_args(
+    myst_kernel_args_t* args,
+    const char* target_name,
+    int argc,
+    const char* argv[],
+    int envc,
+    const char* envp[],
+    const char* cwd,
+    const char* hostname,
+    const void* regions_end,
+    const void* image_data,
+    size_t image_size,
+    size_t max_threads,
+    bool trace_errors,
+    bool trace_syscalls,
+    bool export_ramfs,
+    bool have_syscall_instruction,
+    bool tee_debug_mode,
+    uint64_t thread_event,
+    long (*tcall)(long n, long params[6]),
+    const char* rootfs,
+    char* err,
+    size_t err_size);
+
+#endif /* _MYST_MYST_KARGS_H */
