b9276d84c087c79e3266c1d90aaf80755ed3f414
Xuejun Yang <xuejya@microsoft.com>
2021-03-30 21:49:57 +0000

    Clean up and solutions and make them run faster
    1) Move numpy from solutions/python to solutions/python_webserver
    2) Remove solutions/python
    3) Add Telemetry-related code to aspnet solution
    4) Address missing netstandard.dll issue for dotnet solutions
    5) Run solutions with "-j"
    6) Add several required glibc functions to musl.
    
    Signed-off-by: Xuejun Yang <xuejya@microsoft.com>

--- solutions/aspnet/Startup.cs
+++ solutions/aspnet/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.WorkerService;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,6 +17,18 @@ namespace webapp
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            TelemetryClient client = new TelemetryClient();
+            var aiOptions = new ApplicationInsightsServiceOptions();
+            // Disables adaptive sampling.
+            aiOptions.EnableAdaptiveSampling = false;
+            // Disables QuickPulse (Live Metrics stream).
+            aiOptions.EnableQuickPulseMetricStream = false;
+            services.AddApplicationInsightsTelemetryWorkerService(aiOptions);
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
