fc9467a3cc053bf13aaf56f8cdc15d8a9f13c1db
Bo Zhang (ACC) <zhanb@microsoft.com>
2021-04-30 20:38:42 +0000

    Made Flask/uWSGI web server solution working

--- solutions/python_web_frameworks/flask_app.py
+++ solutions/python_web_frameworks/flask_app.py
@@ -7,4 +7,4 @@ def index():
     return 'Hello from inside Mystikos!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='8000')
+    app.run(debug=False, host='0.0.0.0', port='8000')
