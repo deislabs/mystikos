91faeaffdc2e0e0c0023f3f2c25cb06fcea209ef
Bo Zhang (ACC) <zhanb@microsoft.com>
2021-05-05 12:36:02 +0000

    openmp, tensor flow, pytorch sample worksapce

--- samples/openmp/openmp-test.c
+++ samples/openmp/openmp-test.c
@@ -1,9 +1,9 @@
-/* 
+/*
 Copyright (c) 1997-2019 OpenMP Architecture Review Board.
 All rights reserved.
 
 Permission to redistribute and use without fee all or part of the source
-codes and the associated document (the Software), with or without 
+codes and the associated document (the Software), with or without
 modification, is granted, provided that the following conditions are met:
 
  * Redistributions of the software must retain the above copyright
@@ -14,48 +14,50 @@ modification, is granted, provided that the following conditions are met:
    derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE OPENMP ARCHITECTURE REVIEW BOARD "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE OPENMP ARCHITECTURE REVIEW 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE OPENMP ARCHITECTURE REVIEW
 BOARD BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
 OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-  
 /*
-* @@name:	fpriv_sections.1c
-* @@type:	C
-* @@compilable:	yes
-* @@linkable:	yes
-* @@expect:	success
-*/
+ * @@name:	fpriv_sections.1c
+ * @@type:	C
+ * @@compilable:	yes
+ * @@linkable:	yes
+ * @@expect:	success
+ */
 #include <omp.h>
+#include <openacc.h>
 #include <stdio.h>
 #define NT 4
-int main( ) {
+int main()
+{
     int section_count = 0;
     omp_set_dynamic(0);
     omp_set_num_threads(NT);
-#pragma omp parallel
-#pragma omp sections firstprivate( section_count )
-{
-#pragma omp section
+#pragma omp parallel proc_bind(close)
+#pragma omp sections firstprivate(section_count)
     {
-        section_count++;
-        /* may print the number one or two */
-        printf( "section_count %d\n", section_count );
-    }
 #pragma omp section
-    {
-        section_count++;
-        /* may print the number one or two */
-        printf( "section_count %d\n", section_count );
+        {
+            section_count++;
+            /* may print the number one or two */
+            printf("section_count %d\n", section_count);
+        }
+#pragma omp section
+        {
+            section_count++;
+            /* may print the number one or two */
+            printf("section_count %d\n", section_count);
+        }
     }
-}
+    acc_async_test_all();
     return 0;
 }
