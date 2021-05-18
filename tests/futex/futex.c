23ca07517605258141c3c709c29636b7679a7891
Mike Brasher <mikbras@microsoft.com>
2021-05-18 22:30:27 +0000

    fix futex-wake bug

--- tests/futex/futex.c
+++ tests/futex/futex.c
@@ -2,14 +2,18 @@
 // Licensed under the MIT License.
 
 #include <assert.h>
+#include <limits.h>
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <syscall.h>
 #include <time.h>
 #include <unistd.h>
+#include "../utils/utils.h"
 
 #define FUTEX_WAIT 0
+#define FUTEX_WAKE 1
 
 /* get the timestamp in nanoseconds */
 uint64_t timestamp_nsec(void)
@@ -52,9 +56,55 @@ void test_double_wait(void)
     printf("=== passed test (%s)\n", __FUNCTION__);
 }
 
+static int _uaddr = 0;
+
+static void* _wait_thread(void* arg)
+{
+    long r = syscall(SYS_futex, &_uaddr, FUTEX_WAIT, 1, NULL, NULL, 0);
+    assert(r == 0);
+    return NULL;
+}
+
+static void test_wait_and_wake(void)
+{
+    pthread_t t1;
+
+    _uaddr = 1;
+    assert(pthread_create(&t1, NULL, _wait_thread, NULL) == 0);
+
+    /* wait until thread is asleep */
+    sleep_msec(50);
+
+    _uaddr = 0;
+    long r = syscall(SYS_futex, &_uaddr, FUTEX_WAKE, INT_MAX, NULL, NULL, 0);
+    assert(r == 1);
+    assert(pthread_join(t1, NULL) == 0);
+}
+
+static void test_wait_and_wake_n(void)
+{
+    static const size_t nthreads = 16;
+    pthread_t t[nthreads];
+    _uaddr = 1;
+
+    for (size_t i = 0; i < nthreads; i++)
+        assert(pthread_create(&t[i], NULL, _wait_thread, NULL) == 0);
+
+    /* wait until thread is asleep */
+    sleep_msec(50);
+
+    long r = syscall(SYS_futex, &_uaddr, FUTEX_WAKE, INT_MAX, NULL, NULL, 0);
+    assert(r == 16);
+
+    for (size_t i = 0; i < nthreads; i++)
+        assert(pthread_join(t[i], NULL) == 0);
+}
+
 int main(int argc, const char* argv[])
 {
     test_double_wait();
+    test_wait_and_wake();
+    test_wait_and_wake_n();
 
     printf("=== passed test (%s)\n", argv[0]);
 
