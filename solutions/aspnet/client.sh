1a721a9e433c14bc771312c3731d12ab8b97a314
Mike Brasher <mikbras@microsoft.com>
2021-06-16 23:52:52 +0000

    fix failure to create cpio from pipes

--- solutions/aspnet/client.sh
+++ solutions/aspnet/client.sh
@@ -3,7 +3,7 @@ rm -f client.output
 
 # wait for the server to launch and then run client command
 while read -r line; do
-  echo $line
+  echo "$line"
   if echo $line | grep -q 'Content root path'; then
     curl 127.0.0.1:5050 || exit 1
     curl 127.0.0.1:5050 || exit 1
