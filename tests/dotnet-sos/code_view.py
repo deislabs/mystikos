a74dda0d9ff857a35739d139d48b9bad577ce4a2
Vikas Tikoo <vitikoo@microsoft.com>
2021-06-18 17:45:25 +0000

    Fix to code viewer hook
    Signed-off-by: Vikas Tikoo <vitikoo@microsoft.com>

--- tests/dotnet-sos/code_view.py
+++ tests/dotnet-sos/code_view.py
@@ -1,55 +1,77 @@
-import io, re, sys
+import io, os, re, sys
 import lldb
-from contextlib import redirect_stdout
+
+def lldb_handle_command(debugger, cmd):
+    output = ""
+    old = debugger.GetOutputFile()
+    try:
+        # TODO: Create unique tmp file path per oelldb instance
+        fpath = "/tmp/oelldb.txt"
+        f = open(fpath, "w")
+        debugger.SetOutputFile(f)
+        debugger.SetAsync(False)
+        debugger.HandleCommand(cmd)
+        f.flush()
+        f.close()
+        debugger.SetOutputFile(old)
+        output = open(fpath, "r").read()
+    except:
+        pass
+    debugger.SetOutputFile(old)
+    return output
+
+def lookup_file_in_source_dirs(source_file):
+    source_dirs = os.getenv("DOTNET_SOURCE_DIRS")
+    for s_dir in source_dirs.split(":"):
+        if source_file in os.listdir(s_dir):
+            return s_dir+"/"+source_file
+    return None
 
 def my_stop_hook(debugger, command, result, internal_dict):
     output = ""
-    with io.StringIO() as buf, redirect_stdout(buf):
-        old = debugger.GetOutputFile()
-        debugger.SetOutputFile(buf)
-        debugger.SetAsync(False)
-        stop_reason = (debugger.GetSelectedTarget()
+    stop_reason = (debugger.GetSelectedTarget()
                         .GetProcess()
                         .GetSelectedThread()
                         .GetStopReason())
-        if stop_reason == lldb.eStopReasonBreakpoint:
-            debugger.HandleCommand("re r rip -f x")
-            output = buf.getvalue()
-            buf.truncate(0)
-            m = re.search("rip\\s*=\\s*([0-9a-fA-Fx]+)\.*", output)
-            if m:
-                rip = m[1]
-                debugger.HandleCommand(f"ip2md {rip}")
-                output = buf.getvalue()
-                buf.truncate(0)
-                # TODO: check if this if IP lies in a JITTED frame
-                #print(f"ip2md output: {output}", file=sys.stderr)
-            else:
-                return True  
-            debugger.SetOutputFile(old)
+    if stop_reason == lldb.eStopReasonBreakpoint:
+        output = lldb_handle_command(debugger, "re r rip -f x")
+        m = re.search("\\s*rip\\s*=\\s*([0-9a-fA-Fx]+)\.*", output)
+        if m:
+            rip = m[1]
+            #print("oelldb -> rip = %s" % rip)
+            output = lldb_handle_command(debugger, "ip2md %s" % rip)
+            #print("oelldb -> output = %s" % output)
+            # TODO: check if this if IP lies in a JITTED frame
+            #print(f"ip2md output: {output}", file=sys.stderr)
         else:
             return True
-    print(output)
+    else:
+        return True
+
     # TODO: SOS reads source file info from PDB files,
-    # You can make SOS aware of the pdb file by: setsymbolserver -directory <path-to-dir-containing-pdb> 
+    # You can make SOS aware of the pdb file by: setsymbolserver -directory <path-to-dir-containing-pdb>
     # The source file path is the one recorded at the build time of the pdb file.
     # If its a multi-stage docker build, the file directory structure will not be available.
     # One solution is to read dotnet source directories from an environment variable,
     # and search for the basename part of the file path in those specified directories.
     m = re.search("Source file:\\s*(.+)\\s+@\\s+(\d+)", output)
     if m:
-        filename = m[1]
+        filename_at_build = m[1]
+        basename = filename_at_build.split("/")[-1]
         line = int(m[2])
-        code = open(filename, "r").read().split('\n')
-        K=10
-        for i in range(line-K, line+K):
-            cursor = "->" if i == line else "  "
-            if i >= 0 and i < len(code):
-                print("%3d %s %s" % (i, cursor, code[i]))
+        filename = lookup_file_in_source_dirs(basename)
+        if filename:
+            code = open(filename, "r").read().split('\n')
+            K=10
+            for i in range(line-K, line+K):
+                cursor = "->" if i == line else "  "
+                if i >= 0 and i < len(code):
+                    print("%3d %s %s" % (i, cursor, code[i]))
+        else:
+            print(f"Source file for ${basename} not found in paths specified by  DOTNET_SOURCE_DIRS env variable!")
 
     return True
-    
+
 def __lldb_init_module(debugger, dict):
     debugger.HandleCommand('command script add -f code_view.my_stop_hook my_stop_hook')
     debugger.HandleCommand('target stop-hook add -o "my_stop_hook"')
-
