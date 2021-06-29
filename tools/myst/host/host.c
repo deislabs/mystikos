393acbeb412b69e403be811ead8f544a1f45c873
Mike Brasher <mikbras@microsoft.com>
2021-06-29 00:54:33 +0000

    nosudo version of "myst mkext2"

--- tools/myst/host/host.c
+++ tools/myst/host/host.c
@@ -384,7 +384,7 @@ Where <action> is one of:\n\
 \n\
 "
 
-static int _main(int argc, const char* argv[], const char* envp[])
+int main(int argc, const char* argv[], const char* envp[])
 {
     if (set_program_file(argv[0]) == NULL)
     {
@@ -462,36 +462,4 @@ static int _main(int argc, const char* argv[], const char* envp[])
         fprintf(stderr, USAGE, argv[0]);
         return 1;
     }
-}
-
-int main(int argc, const char* argv[], const char* envp[])
-{
-#ifdef MYST_ENABLE_GCOV
-    const char* uid_str = getenv("SUDO_UID");
-    const char* gid_str = getenv("SUDO_GID");
-    uid_t uid = UINT_MAX;
-    gid_t gid = UINT_MAX;
-
-    /* if running as SUDO, then save the uid and gid */
-    if (uid_str && gid_str)
-    {
-        uid = atoi(uid_str);
-        gid = atoi(gid_str);
-    }
-#endif
-
-    int ec = _main(argc, argv, envp);
-
-#ifdef MYST_ENABLE_GCOV
-    /* if running as SUDO, then restore uid and gid for gcov */
-    if (uid_str && gid_str)
-    {
-        setgid(gid);
-        setegid(gid);
-        setuid(uid);
-        seteuid(uid);
-    }
-#endif
-
-    return ec;
 }
