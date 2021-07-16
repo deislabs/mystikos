25652d1e84583dda5fa7306696204e95cd616062
Mike Brasher <mikbras@microsoft.com>
2021-07-16 15:59:20 +0000

    cleanup

--- include/myst/asyncsyscall.h
+++ include/myst/asyncsyscall.h
@@ -6,12 +6,12 @@
 
 #include <stddef.h>
 
-// Perform an fd-oriented "asynchronous" syscall that can be interrupted by
-// calling myst_interrupt_async_syscall().
+// Perform an fd-oriented syscall that can be asynchronously interrupted by
+// myst_interrupt_async_syscall().
 long myst_async_syscall(long num, int poll_flags, int fd, ...);
 
 // Interrupt the invocation of myst_async_syscall() that is currently blocked
-// on the given fd, forcing myst_async_syscall() to return -EINTR.
+// on the given fd causing it to return -EINTR.
 long myst_interrupt_async_syscall(int fd);
 
 #endif /* _MYST_ASYNFDOPS_H */
