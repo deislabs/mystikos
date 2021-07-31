d90042fb7b65c66d6ebde43d107bf565a106b1cc
Mike Brasher <mikbras@microsoft.com>
2021-07-31 01:06:04 +0000

    add redis back

--- samples/redis/kill.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pid=$(ps -eaf | grep redis | grep -v grep | awk '{ print $2 }')
-if [ ! -z "${pid}" ]; then
-    sudo kill -9 ${pid}
-    echo "killed"
-    exit 0
-fi
-echo "nothing to kill"
