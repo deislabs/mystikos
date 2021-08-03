479381e7873565d371d52a2003d5321ebb2c0edc
salsal97 <salonigoel97@gmail.com>
2021-08-03 22:12:44 +0000

    converted redis sample to solution

--- solutions/redis/kill.sh
+++ solutions/redis/kill.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pid=$(ps -eaf | grep redis | grep -v grep | awk '{ print $2 }')
+pid=$(ps -eaf | grep redis-server | grep -v grep | awk '{ print $2 }')
+ps -eaf | grep redis
 if [ ! -z "${pid}" ]; then
     sudo kill -9 ${pid}
     echo "killed"
