7a2eb3b44ce02c1b2367d77e2ec67a3a2b981b54
Bo Zhang (ACC) <zhanb@microsoft.com>
2021-08-06 00:27:34 +0000

    Change .NET coreclr test suite configuration Break the coreclr tests to mutiple sets Set test result as failed if less than 95% of the test cases passed
    Signed-off-by: Bo Zhang (ACC) <zhanb@microsoft.com>

--- solutions/coreclr/run-single-test.sh
+++ solutions/coreclr/run-single-test.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 
-USAGE="$0 path-to-myst exec|exec-sgx|exec-linux ext2|cpio <path-to-test-dll>"
-HEAP_SIZE="1G"
+USAGE="$0 path-to-myst null|exec-sgx|exec-linux config.json timeout ext2|cpio|package <path-to-test-dll>"
 ret_val=-1
 t0=$(date +"%s")
-TIMEOUT=15
+TIMEOUT=$4
 
-if [[ "$3" == "ext2" ]]; then
+if [[ "$5" == "package" ]]; then
+    timeout --kill-after="${TIMEOUT}s" --signal=KILL $TIMEOUT \
+        $1 \
+        /coreclr-tests-all/$6 \
+        --rootfs=ext2fs > /dev/null 2>&1
+elif [[ "$5" == "ext2" ]]; then
     timeout --kill-after="${TIMEOUT}s" --signal=KILL $TIMEOUT \
         $1 $2 ext2fs \
-        --app-config-path=config.json \
-        --roothash=roothash \
+        --app-config-path=$3 \
         /coreclr-tests-all/Tests/Core_Root/corerun \
-        /coreclr-tests-all/$4 > /dev/null 2>&1
-elif [[ "$3" == "cpio" ]]; then
+        /coreclr-tests-all/$6 > /dev/null 2>&1
+elif [[ "$5" == "cpio" ]]; then
     timeout --kill-after="${TIMEOUT}s" --signal=KILL $TIMEOUT \
         $1 $2 rootfs \
-        --app-config-path=config.json \
+        --app-config-path=$3 \
         /coreclr-tests-all/Tests/Core_Root/corerun \
-        /coreclr-tests-all/$4 > /dev/null 2>&1
+        /coreclr-tests-all/$6 > /dev/null 2>&1
 else
-    echo "$3 is not a supported fs."
+    echo "$5 is not a supported fs."
     echo $USAGE
     exit 1
 fi
 
 ret_val=$?
 t1=$(date +"%s")
-echo "Test: $4 Exit code: $ret_val time taken: $(( $t1 - $t0 )) secs."
+echo "Test: $6 Exit code: $ret_val time taken: $(( $t1 - $t0 )) secs."
 
 # Error code 100 represents success in dotnet runtime tests
 if [[ $ret_val == 100 ]]
 then
-    echo $4 >> PASSED
+    echo $6 >> PASSED
 else
-    echo $4 >> FAILED-$ret_val
+    echo $6 >> FAILED-$ret_val
 fi
 
 
--- solutions/coreclr/test-runner.sh
+++ solutions/coreclr/test-runner.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-USAGE="$0 path-to-myst exec|exec-sgx|exec-linux ext2|cpio <test_list_file>"
+USAGE="$0 path-to-myst null|exec-sgx|exec-linux config.json timeout ext2|cpio|package <test_list_file>"
 NPROCS=$(grep -c ^processor /proc/cpuinfo)
-NPROCS=4
-if [[ "$3" != "ext2" ]]; then
-	if [[ "$3" != "cpio" ]]; then
-		echo "Unsupported fs: $3"
-		echo $USAGE
-		exit 1
+NPROCS=1
+if [[ "$5" != "package" ]]; then
+	if [[ "$5" != "ext2" ]]; then
+		if [[ "$5" != "cpio" ]]; then
+			echo "Unsupported mode: $5"
+			echo $USAGE
+			exit 1
+		fi
 	fi
 fi
 
-if [[ "$#" == 4 ]]; then
-	mapfile -t TEST_LIST < $4
+if [[ "$5" != "package" ]]; then
+	NPROCS=1
+fi
+
+if [[ "$#" == 6 ]]; then
+	mapfile -t TEST_LIST < $6
 else
 	mapfile -t TEST_LIST < pr0-PASSED
 fi
@@ -29,7 +35,7 @@ do
 	chunk=( "${TEST_LIST[@]:i:NPROCS}" )
 	for test in ${chunk[*]}
 	do
-		./run-single-test.sh $1 $2 $3 $test &
+		./run-single-test.sh $1 $2 $3 $4 $5 $test &
 	done
 	wait
 	# if any test failed, mystikos wasn't able to clean
@@ -40,36 +46,3 @@ end_time=$(date +"%s")
 elapsed_secs=$(( end_time - start_time ))
 echo "****************************************"
 echo "Time elapsed $elapsed_secs seconds"
-
-# Print pass/fail statistics
-pass_count=0
-if [[ -f PASSED ]]; then
-	pass_count=$(wc -l PASSED | awk '{ print $1 }')
-	echo "Tests passed: $pass_count"
-	echo "Pass percentage: $(( $pass_count*100/$NUM_TESTS )) %"
-	cat PASSED
-fi
-
-if [[ -f FAILED-139 ]]; then
-	echo "****************************************"
-	echo "Seg fault count: $(wc -l FAILED-139)"
-	cat FAILED-139
-fi
-
-if [[ -f FAILED-134 ]]; then
-	echo "****************************************"
-	echo "Program abort count: $(wc -l FAILED-134)"
-	cat FAILED-134
-fi
-
-if [[ -f FAILED-137 ]]; then
-	echo "****************************************"
-	echo "Timed out and Killed count: $(wc -l FAILED-137)"
-	cat FAILED-137
-fi
-
-if [[ -f FAILED-1 ]]; then
-	echo "****************************************"
-	echo "General error count: $(wc -l FAILED-1)"
-	cat FAILED-1
-fi
