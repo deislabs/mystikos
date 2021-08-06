8f70204d39990c206877c33bef4ea803f5991414
Bo Zhang (ACC) <zhanb@microsoft.com>
2021-08-06 00:27:34 +0000

    Change .NET coreclr test suite configuration Break the coreclr tests to mutiple sets Set test result as failed if less than 95% of the test cases passed
    Signed-off-by: Bo Zhang (ACC) <zhanb@microsoft.com>

---
+++ solutions/coreclr/stat.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Print pass/fail statistics
+NUM_TESTS=$(wc -l pr0-tests-all | awk '{ print $1 }')
+pass_count=0
+if [[ -f PASSED ]]; then
+	pass_count=$(wc -l PASSED | awk '{ print $1 }')
+	num_tests=pass_count
+	echo "Tests passed: $pass_count"
+	echo "Pass percentage: $(( $pass_count*100/$NUM_TESTS )) %"
+	cat PASSED
+fi
+
+if [[ -f FAILED-139 ]]; then
+	echo "****************************************"
+	echo "Seg fault count: $(wc -l FAILED-139)"
+	cat FAILED-139
+fi
+
+if [[ -f FAILED-134 ]]; then
+	echo "****************************************"
+	echo "Program abort count: $(wc -l FAILED-134)"
+	cat FAILED-134
+fi
+
+if [[ -f FAILED-137 ]]; then
+	echo "****************************************"
+	echo "Timed out and Killed count: $(wc -l FAILED-137)"
+	cat FAILED-137
+fi
+
+if [[ -f FAILED-1 ]]; then
+	echo "****************************************"
+	echo "General error count: $(wc -l FAILED-1)"
+	cat FAILED-1
+fi
+
+if [ "$(( pass_count*100/NUM_TESTS ))" -lt "95" ]; then
+	echo "Failed to pass at least 95% - Pass percentage: $(( $pass_count*100/$NUM_TESTS ))%"
+	exit 1
+fi
