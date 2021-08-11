6978633fc811318875aed75e9a4b60607a979867
Mike Brasher <mikbras@microsoft.com>
2021-08-11 23:58:24 +0000

    reimplement pthread_getattr_np()

---
+++ crt/pthread.c
@@ -0,0 +1,41 @@
+#define hidden __attribute__((__visibility__("hidden")))
+
+#include <myst/syscallext.h>
+#include <pthread_impl.h>
+#include <stdio.h>
+#include <string.h>
+#include <syscall.h>
+
+// Reimplement pthread_getattr_np() using the SYS_get_process_thread_stack
+// extended syscall. The musl libc version is less efficient since it uses
+// mremap() probing to find the guard page. Also, the musl libc version will
+// not work with Mystikos since the stack is obtained with malloc() rather
+// than mmap().
+int pthread_getattr_np(pthread_t thread, pthread_attr_t* attr)
+{
+    uintptr_t stackaddr = 0;
+    size_t stacksize = 0;
+
+    if (!thread || !attr)
+        return EINVAL;
+
+    if (thread->stack) /* this thread was created by pthread_create() */
+    {
+        stackaddr = (uintptr_t)thread->stack;
+        stacksize = thread->stack_size;
+    }
+    else /* this is the main thread and was created by the program loader */
+    {
+        if (syscall(SYS_get_process_thread_stack, &stackaddr, &stacksize) != 0)
+            return ENOSYS;
+        stackaddr += stacksize;
+    }
+
+    *attr = (pthread_attr_t){0};
+    attr->_a_detach = (thread->detach_state >= DT_DETACHED) ? 1 : 0;
+    attr->_a_guardsize = thread->guard_size;
+    attr->_a_stackaddr = stackaddr;
+    attr->_a_stacksize = stacksize;
+
+    return 0;
+}
---
+++ tests/stacksize/stacksize.c
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#define _GNU_SOURCE
+#include <assert.h>
+#include <limits.h>
+#include <myst/syscallext.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/auxv.h>
+#include <unistd.h>
+
+int main(int argc, const char* argv[])
+{
+    register uint64_t sp __asm__("rsp");
+    void* stack1;
+    size_t size1;
+    void* stack2;
+    size_t size2;
+
+    /* Get the stack and stack size with an extended syscall */
+    assert(syscall(SYS_get_process_thread_stack, &stack1, &size1) == 0);
+
+    /* Get the stack and stack size with the pthread interface */
+    {
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        assert(pthread_getattr_np(pthread_self(), &attr) == 0);
+        assert(pthread_attr_getstack(&attr, &stack2, &size2) == 0);
+        pthread_attr_destroy(&attr);
+    }
+
+    printf("[%p:%zu]\n", stack2, size2);
+    printf("[%p:%zu]\n", stack1, size1);
+    assert(stack1 == stack2);
+    assert(size1 == size2);
+
+    printf("=== passed test (%s)\n", argv[0]);
+
+    return 0;
+}
