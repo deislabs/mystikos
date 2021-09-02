e68e06b2af00fa37563a8f5343d3dce252238611
salsal97 <salonigoel97@gmail.com>
2021-09-02 17:11:19 +0000

    updating aspnet to kill server based on running process data

--- solutions/aspnet/client.sh
+++ solutions/aspnet/client.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 rm -f client.output
 
-# wait for the server to launch and then run client command
-while read -r line; do
-  echo "$line"
-  if echo $line | grep -q 'Content root path'; then
-    curl 127.0.0.1:5050 || exit 1
-    sleep 1
-    curl 127.0.0.1:5050 || exit 1
-    sleep 1
-    curl 127.0.0.1:5050 || exit 1
-    sleep 1
-    curl 127.0.0.1:5050 || exit 1
-    sleep 1
-    curl 127.0.0.1:5050 || exit 1
-    sleep 1
-    touch client.output
-    exit 0
-  fi
-done
+curl 127.0.0.1:5050 || exit 1
+sleep 1
+curl 127.0.0.1:5050 || exit 1
+sleep 1
+curl 127.0.0.1:5050 || exit 1
+sleep 1
+curl 127.0.0.1:5050 || exit 1
+sleep 1
+curl 127.0.0.1:5050 || exit 1
+sleep 1
+touch client.output
+exit 0
