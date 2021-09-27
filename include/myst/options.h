6d66e76f552d187ab097ac0c298a17f2c859450a
Mike Brasher <mikbras@microsoft.com>
2021-09-27 21:57:41 +0000

    paul.cnfig

--- include/myst/options.h
+++ include/myst/options.h
@@ -6,15 +6,17 @@
 
 #include <limits.h>
 
+#include <myst/args.h>
 #include <myst/kernel.h>
 #include <myst/types.h>
 
+// common options between sgx and linux target
 typedef struct myst_options
 {
-    bool trace_errors;
-    bool trace_syscalls;
     bool have_syscall_instruction;
     bool have_fsgsbase_instructions;
+    bool trace_errors;
+    bool trace_syscalls;
     bool shell_mode;
     bool debug_symbols;
     bool memcheck;
@@ -26,9 +28,17 @@ typedef struct myst_options
     size_t max_affinity_cpus;
     char rootfs[PATH_MAX];
     myst_fork_mode_t fork_mode;
-
     myst_host_enc_uid_gid_mappings host_enc_uid_gid_mappings;
 } myst_options_t;
+
+typedef struct myst_final_options
+{
+    myst_options_t base;
+    const char* __cwd;
+    const char* __hostname;
+    myst_args_t args;
+    myst_args_t env;
+} myst_final_options_t;
 
 extern myst_options_t __options;
 
