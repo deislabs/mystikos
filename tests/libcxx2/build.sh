62ee3e6dc5bdac561c8d16a4c496b1ecde585809
salsal97 <salonigoel97@gmail.com>
2021-10-06 23:08:31 +0000

    updated makefiles

--- tests/libcxx2/build.sh
+++ tests/libcxx2/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -x
+rm appdir2/test
 g++ -std=c++17 test.cpp -pthread -fPIC -o test
 cp test appdir2
 rm rootfs2
-# ../../build/bin/myst mkext2 appdir2 rootfs2
-../../build/bin/myst exec-sgx --nobrk appdir2 /test
+../../build/bin/myst mkext2 appdir2 rootfs2
+../../build/bin/myst exec-sgx --nobrk rootfs2 /test
 set +x
