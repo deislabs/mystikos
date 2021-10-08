a95d3c9a465730dbb1f412942289c379bab50b21
Rathna Ramesh <rathna1993@gmail.com>
2021-10-08 18:38:03 +0000

    solutions/coreclr-p1 initial commit
    Signed-off-by: Rathna Ramesh <rathna1993@gmail.com>

---
+++ solutions/coreclr-p1/run-single-test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USAGE="$0 path-to-myst null|exec-sgx|exec-linux config.json timeout ext2|cpio|package <path-to-test-dll> \"<args>\""
+ret_val=-1
+t0=$(date +"%s")
+TIMEOUT=$4
+
+if [[ "$5" == "package" ]]; then
+    timeout --kill-after="${TIMEOUT}s" --signal=KILL $TIMEOUT \
+        $1 \
+        /coreclr-tests-all/$6 $7 --rootfs=ext2fs >/dev/null 2>&1
+elif [[ "$5" == "ext2" ]]; then
+    timeout --kill-after="${TIMEOUT}s" --signal=KILL $TIMEOUT \
+        $1 $2 ext2fs \
+        --app-config-path=$3 \
+        /coreclr-tests-all/Tests/Core_Root/corerun \
+        /coreclr-tests-all/$6 $7> /dev/null 2>&1
+elif [[ "$5" == "cpio" ]]; then
+    timeout --kill-after="${TIMEOUT}s" --signal=KILL $TIMEOUT \
+        $1 $2 rootfs \
+        --app-config-path=$3 \
+        /coreclr-tests-all/Tests/Core_Root/corerun \
+        /coreclr-tests-all/$6 $7> /dev/null 2>&1
+else
+    echo "$5 is not a supported fs."
+    echo $USAGE
+    exit 1
+fi
+
+ret_val=$?
+t1=$(date +"%s")
+echo "Test: $6 Exit code: $ret_val time taken: $(( $t1 - $t0 )) secs."
+
+# Error code 100 represents success in dotnet runtime tests
+if [[ $ret_val == 100 ]]
+then
+    echo $6 >> PASSED
+else
+    echo $6 >> FAILED-$ret_val
+fi
+
+
---
+++ solutions/coreclr-p1/test-runner.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USAGE="$0 path-to-myst null|exec-sgx|exec-linux config.json timeout ext2|cpio|package <test_list_file> <arg1>"
+NPROCS=$(grep -c ^processor /proc/cpuinfo)
+
+if [[ "$5" != "package" ]]; then
+	if [[ "$5" != "ext2" ]]; then
+		if [[ "$5" != "cpio" ]]; then
+			echo "Unsupported mode: $5"
+			echo $USAGE
+			exit 1
+		fi
+	fi
+fi
+
+if [[ "$5" != "package" ]]; then
+	NPROCS=1
+fi
+
+if [[ "$#" -ge 6 ]]; then
+	mapfile -t TEST_LIST < $6
+else
+	mapfile -t TEST_LIST < pr0-PASSED
+fi
+
+idx=1
+NUM_TESTS=${#TEST_LIST[@]}
+echo "Running $NUM_TESTS tests."
+echo "Parallelism: $NPROCS"
+start_time=$(date +"%s")
+for((i=0; i < ${#TEST_LIST[@]}; i+=NPROCS))
+do
+	echo "****************************************"
+	echo "Run test $(( $i+1 )) - $(( $i+$NPROCS ))"
+	chunk=( "${TEST_LIST[@]:i:NPROCS}" )
+	for test in ${chunk[*]}
+	do
+		./run-single-test.sh $1 $2 $3 $4 $5 $test $7 &
+	done
+	wait
+	# if any test failed, mystikos wasn't able to clean
+	# the tmp files, cleanup files
+	sudo rm -rf /tmp/myst*
+done
+end_time=$(date +"%s")
+elapsed_secs=$(( end_time - start_time ))
+echo "****************************************"
+echo "Time elapsed $elapsed_secs seconds"
