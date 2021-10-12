8d927c9d109792c11d50c6dd0fa0f8341d7f3a32
Xuejun Yang <xuejya@microsoft.com>
2021-10-12 17:51:16 +0000

    Fix pytorch solution on the nightly pipeline
    Signed-off-by: Xuejun Yang <xuejya@microsoft.com>

--- solutions/pytorch_inference/kill.sh
+++ solutions/pytorch_inference/kill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pid=$(ps -eaf | grep "${1}\|${2}" | grep ext2rootfs | awk '{ print $2 }')
+pid=$(ps -eaf | grep "${1}\|${2}" | grep rootfs | awk '{ print $2 }')
 if [ ! -z "${pid}" ]; then
     kill -9 ${pid}
 fi
