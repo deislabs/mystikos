c3638cc16b6c8cb3f57ae849d1054896248342c9
Mike Brasher <mikbras@microsoft.com>
2021-10-13 20:28:57 +0000

    comments

--- include/myst/config.h
+++ include/myst/config.h
@@ -4,11 +4,12 @@
 #ifndef _MYST_CONFIG_H
 #define _MYST_CONFIG_H
 
+/* enable interruption of kernel threads blocked on host */
 #define MYST_INTERRUPT_WITH_SIGNAL 1
 
+/* select interruption for nanosleep(), poll(), and epoll() */
 #if (MYST_INTERRUPT_WITH_SIGNAL == 1)
-/* ATTN: disable nanosleep() interrupt for now */
-#define MYST_INTERRUPT_NANOSLEEP_WITH_SIGNAL -1
+#define MYST_INTERRUPT_NANOSLEEP_WITH_SIGNAL -1 /* disabled initially */
 #define MYST_INTERRUPT_POLL_WITH_SIGNAL 1
 #define MYST_INTERRUPT_EPOLL_WITH_SIGNAL 1
 #else
@@ -17,6 +18,7 @@
 #define MYST_INTERRUPT_EPOLL_WITH_SIGNAL -1
 #endif
 
+/* enable tracing of EINTR returns from nanosleep(), poll(), and epoll() */
 #define MYST_TRACE_THREAD_INTERRUPTIONS 1
 
 #endif /* _MYST_CONFIG_H */
