a158b2dd3966c064ec70d42d0f951461b3d46f7c
salsal97 <salonigoel97@gmail.com>
2021-10-13 17:10:39 +0000

    added sample for running mystikos on kubernetes

---
+++ samples/aks_sample/hello.c
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char* argv[])
+{
+    assert(argc == 4);
+    // assert(strcmp(argv[0], "./myst/bin/hello") == 0);
+    assert(strcmp(argv[1], "red") == 0);
+    assert(strcmp(argv[2], "green") == 0);
+    assert(strcmp(argv[3], "blue") == 0);
+    assert(argv[4] == NULL);
+
+    printf("  Hello world!\n  I received: ");
+    for (int i = 0; i < argc; i++)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("argv[%d]={%s}", i, argv[i]);
+    }
+
+    printf("\n=== passed test (%s)\n", argv[0]);
+
+    return 0;
+}
