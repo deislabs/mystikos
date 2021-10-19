a54975f273ab7c2591ee9fa5ef036734ea846d89
Mike Brasher <mikbras@microsoft.com>
2021-10-19 23:41:31 +0000

    fix flaky clock test

--- tests/clock/main.c
+++ tests/clock/main.c
@@ -23,7 +23,7 @@ static int is_sgx_target()
 static int test_clock_get_time(long reference)
 {
     struct timespec tp = {0};
-    long tolerance = NANO_IN_SECOND * 2; // 2s
+    long tolerance = NANO_IN_SECOND * 3L; // 3s
 
 #ifdef MYST_ENABLE_GCOV
     /* gcov slows down the app, so add another 1/2 second to the tolerance */
