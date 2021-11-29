a243498657925332eff8a7cb8518900e5f4efa01
Chris Yan <chrisyan@microsoft.com>
2021-11-29 10:46:24 -0800

    UAMI enabled agents
    Signed-off-by: Chris Yan <chrisyan@microsoft.com>

--- .jenkins/scripts/solutions/azure-config.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install -y azure-cli
-az login --service-principal -u $SERVICE_PRINCIPAL_ID -p $SERVICE_PRINCIPAL_PASSWORD --tenant $TENANT_ID >> /dev/null
-az account set -s $AZURE_SUBSCRIPTION_ID
-az vm identity assign -g $JENKINS_RESOURCE_GROUP -n $(hostname) --identities $MYSTIKOS_MANAGED_ID >> /dev/null
-az vm update -g $JENKINS_RESOURCE_GROUP -n $(hostname) --set identity.type='UserAssigned' >> /dev/null
