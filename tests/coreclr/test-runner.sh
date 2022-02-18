d44d1bd1f4c84387ac3571787b5e4feaea8cf263
Vikas Tikoo <vitikoo@microsoft.com>
2022-02-18 16:08:33 +0000

    Run large memory tests in serial
    Signed-off-by: Vikas Tikoo <vitikoo@microsoft.com>

--- tests/coreclr/test-runner.sh
+++ tests/coreclr/test-runner.sh
@@ -2,7 +2,7 @@
 
 USAGE="$0 path-to-myst null|exec-sgx|exec-linux "
 USAGE="config.json timeout ext2|cpio|package "
-USAGE="<test_list_file> <expected-exitcode> <parallelism>"
+USAGE="<test_list_file> <parallelism> <expected-exitcode>"
 PARALLELISM=$(grep -c ^processor /proc/cpuinfo)
 
 if [[ "$5" != "package" ]]; then
@@ -15,22 +15,22 @@ if [[ "$5" != "package" ]]; then
 	fi
 fi
 
-if [[ "$#" -ge 8 ]]; then
-	PARALLELISM=$8
+if [[ "$#" -ge 7 ]]; then
+	PARALLELISM=$7
 fi
 
+TEST_LIST_FILE=${PWD}/pr0-PASSED
 if [[ "$#" -ge 6 ]]; then
-	mapfile -t TEST_LIST < $6
-else
-	mapfile -t TEST_LIST < ${PWD}/pr0-PASSED
+	TEST_LIST_FILE=$6
 fi
+mapfile -t TEST_LIST < $TEST_LIST_FILE
 
 SCRIPT=`realpath -s $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 idx=1
 NUM_TESTS=${#TEST_LIST[@]}
-echo "Running $NUM_TESTS tests."
+echo "Running $NUM_TESTS tests from $TEST_LIST_FILE."
 echo "Parallelism: $PARALLELISM"
 start_time=$(date +"%s")
 for((i=0; i < ${#TEST_LIST[@]}; i+=PARALLELISM))
@@ -40,11 +40,11 @@ do
 	chunk=( "${TEST_LIST[@]:i:PARALLELISM}" )
 	for test in "${chunk[@]}"
 	do
-		${SCRIPTPATH}/run-single-test.sh $1 $2 $3 $4 $5 "$test" $7 &
+		${SCRIPTPATH}/run-single-test.sh $1 $2 $3 $4 $5 "$test" $8 &
 	done
 	wait
 done
 end_time=$(date +"%s")
 elapsed_secs=$(( end_time - start_time ))
 echo "****************************************"
-echo "Time elapsed $elapsed_secs seconds"
+echo "Time elapsed $elapsed_secs seconds ($TEST_LIST_FILE)"
