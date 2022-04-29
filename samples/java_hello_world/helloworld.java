342afcc84b713ba72c8fa36a7f6f2e7e42d4e10e
Radhika Jandhyala <radhikaj@microsoft.com>
2022-04-28 20:39:58 -0700

    Update helloworld.java

--- samples/java_hello_world/helloworld.java
+++ samples/java_hello_world/helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld test from Java Application running in Mystikos.");
+        System.out.println("HelloWorld test from Java Application running in Mystikos. ***");
     }
 }
