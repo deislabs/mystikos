1497501e1d20eb94f8cb401333d972822b6d9d07
Vikas Tikoo <vikasamar@gmail.com>
2022-06-06 23:49:46 +0000

    Bring mmap, munmap, mremap syscall handlers under recursive mman lock
    Signed-off-by: Vikas Tikoo <vikasamar@gmail.com>

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -3687,6 +3687,7 @@ static long _SYS_mmap(long n, long params[6], const myst_process_t* process)
     }
 
     /* this can return (void*)-errno */
+    myst_mman_lock();
     long ret = (long)myst_mmap(addr, length, prot, flags, fd, offset);
 
     // ATTN : temporary workaround for myst_mmap()  inaccurate return
@@ -3702,10 +3703,14 @@ static long _SYS_mmap(long n, long params[6], const myst_process_t* process)
 
         /* set ownership this mapping to pid */
         if (myst_mman_pids_set(ptr, length, pid) != 0)
+        {
+            myst_mman_unlock();
             myst_panic("myst_mman_pids_set()");
+        }
 
         ret = (long)ptr;
     }
+    myst_mman_unlock();
 
     return (_return(n, ret));
 }
@@ -3775,14 +3780,19 @@ static long _SYS_munmap(
     if (!myst_process_owns_mem_range(addr, length, true))
         return (_return(n, -EINVAL));
 
+    myst_mman_lock();
     long ret = (long)myst_munmap(addr, length);
 
     if (ret == 0)
     {
         /* set ownership this mapping to nobody */
         if (myst_mman_pids_set(addr, length, 0) != 0)
+        {
+            myst_mman_unlock();
             myst_panic("myst_mman_pids_set()");
+        }
     }
+    myst_mman_unlock();
 
     return (_return(n, ret));
 }
@@ -3959,6 +3969,7 @@ static long _SYS_mremap(long n, long params[6])
               myst_process_owns_mem_range(old_address, old_size, NULL)))
         return (_return(n, -EINVAL));
 
+    myst_mman_lock();
     ret =
         (long)myst_mremap(old_address, old_size, new_size, flags, new_address);
 
@@ -3968,12 +3979,19 @@ static long _SYS_mremap(long n, long params[6])
 
         /* set ownership of old mapping to nobody */
         if (myst_mman_pids_set(old_address, old_size, 0) != 0)
+        {
+            myst_mman_unlock();
             myst_panic("myst_mman_pids_set()");
+        }
 
         /* set ownership of new mapping to pid */
         if (myst_mman_pids_set((const void*)ret, new_size, pid) != 0)
+        {
+            myst_mman_unlock();
             myst_panic("myst_mman_pids_set()");
+        }
     }
+    myst_mman_unlock();
 
     return (_return(n, ret));
 }
