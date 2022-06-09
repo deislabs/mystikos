12088eaeb1088671058b72d744a5cad118536fbc
Zijie Wu <zijiewu@microsoft.com>
2022-06-09 21:39:34 +0000

    Fix unbounded write

--- tests/procfs/maps.c
+++ tests/procfs/maps.c
@@ -1,1 +1,21 @@
 #include "../../host/maps.c"
+
+size_t myst_strlcpy(char* dest, const char* src, size_t size)
+{
+    const char* start = src;
+
+    if (size)
+    {
+        char* end = dest + size - 1;
+
+        while (*src && dest != end)
+            *dest++ = (char)*src++;
+
+        *dest = '\0';
+    }
+
+    while (*src)
+        src++;
+
+    return (size_t)(src - start);
+}
