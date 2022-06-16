8cfbf46eec5641b1af388e85a35a9be3a6ae2b26
Chris Yan <chrisyan@microsoft.com>
2022-06-16 10:52:33 -0700

    Add error checking and retries
    Signed-off-by: Chris Yan <chrisyan@microsoft.com>

--- .jenkins/scripts/global/init-install.sh
+++ .jenkins/scripts/global/init-install.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
 sudo apt-get install \
   build-essential    \
   lcov               \
--- .jenkins/scripts/global/make-world.sh
+++ .jenkins/scripts/global/make-world.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
 sudo rm -rf $(git ls-files --others --directory)
 make distclean
 make -j world
