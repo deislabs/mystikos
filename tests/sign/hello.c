9991e3bd53bd7c1ce7b71eb592239b4310f1e274
Mike Brasher <mikbras@microsoft.com>
2023-02-10 01:20:11 +0000

    Fix broken sigining of EXT2 rootfs

---
+++ tests/sign/hello.c
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, const char* argv[])
+{
+    printf("Hello!\n");
+    printf("=== passed test (%s)\n", argv[0]);
+
+    return 0;
+}
